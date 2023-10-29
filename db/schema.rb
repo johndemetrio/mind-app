ad0609aa408364d75c9efffdf959b97873a644fb
John Demetrio <johndemetrio123@gmail.com>
2023-10-29 11:59:20 -0300

    adicionando model Tag com has_many no model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_142826) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_29_144629) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "contents", force: :cascade do |t|
@@ -21,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_142826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.citext "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "user_id"], name: "index_tags_on_name_and_user_id", unique: true
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_142826) do
   end
 
   add_foreign_key "contents", "users"
+  add_foreign_key "tags", "users"
 end
