c00edea7e4fc9be74aef5387e108934f40681304
John Demetrio <johndemetrio123@gmail.com>
2023-10-29 12:13:42 -0300

    adicionando validações no model tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_144629) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_29_150321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_144629) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.citext "name"
+    t.citext "name", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
