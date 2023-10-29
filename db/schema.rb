5a78ee1757f4472bfb5a7be73cde95129b8b0901
John Demetrio <johndemetrio123@gmail.com>
2023-10-29 14:30:04 -0300

    adicionando model TagContent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_150321) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_29_151425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_150321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tag_contents", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "content_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["content_id"], name: "index_tag_contents_on_content_id"
+    t.index ["tag_id"], name: "index_tag_contents_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -46,5 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_150321) do
   end
 
   add_foreign_key "contents", "users"
+  add_foreign_key "tag_contents", "contents"
+  add_foreign_key "tag_contents", "tags"
   add_foreign_key "tags", "users"
 end
