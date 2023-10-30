69d7c3b338daf896a25455e721f7086689c52366
John Demetrio <johndemetrio123@gmail.com>
2023-10-30 20:17:51 -0300

    arrumando delete do model tag e content

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,6 @@ class Tag < ApplicationRecord
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
 
-  has_many :tag_content
+  has_many :tag_content, dependent: :destroy
   has_many :contents, through: :tag_contents
 end
