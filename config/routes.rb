472ec20ca168e30980783bca958cc8774e02166b
John Demetrio <johndemetrio123@gmail.com>
2023-11-01 19:43:48 -0300

    adicionando controller contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
-  get 'home/index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'home', to: 'home#index'
+
+  resources :contents
 end
