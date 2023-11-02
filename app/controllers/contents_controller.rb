328af6a44d5db69e1ec1f0709af464b6daa4e0e2
John Demetrio <johndemetrio123@gmail.com>
2023-11-01 22:12:02 -0300

    adicionando form contents

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,11 +1,26 @@
 class ContentsController < ApplicationController
     before_action :authenticate_user!
     def index
-        @contents = current_user.contents
+      @contents = current_user.contents
     end
 
     def new
-        
+      @content = Content.new
+    end
+
+    def create
+      @content = current_user.contents.build(content_params)
+      if @content.save
+        redirect_to contents_path, notice: 'Content created!'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def content_params
+      params.require(:content).permit(:title, :description)
     end
   end
   
